17256b5aa759989ec435c7b382d744290f3646e3
davidokun <davidokun99@gmail.com>
2018-03-29 14:11:20 -0500

    Exposing Flux<Quotes> as a web service

--- webflux-stock-quotes/src/main/java/com/singletonapps/webflux/webfluxstockquotes/QuoteHandler.java
+++ webflux-stock-quotes/src/main/java/com/singletonapps/webflux/webfluxstockquotes/QuoteHandler.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.http.MediaType.TEXT_PLAIN;
+import java.time.Duration;
+
+import static org.springframework.http.MediaType.*;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class QuoteHandler {
+
+    private final Flux<Quote> quoteStream;
+
+    public QuoteHandler(QuoteGenerator quoteGenerator) {
+        this.quoteStream = quoteGenerator.fetchQuoteStream(Duration.ofMillis(1000)).share();
+    }
 
     public Mono<ServerResponse> hello(ServerRequest request) {
 
@@ -22,6 +31,20 @@ public class QuoteHandler {
 
         return ok().contentType(TEXT_PLAIN)
                 .body(request.bodyToMono(String.class), String.class);
+    }
+
+    public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+
+        return ok().contentType(APPLICATION_STREAM_JSON)
+                .body(this.quoteStream, Quote.class);
+    }
+
+    public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+
+        int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+
+        return ok().contentType(APPLICATION_JSON)
+                .body(this.quoteStream.take(size), Quote.class);
     }
 
 }
--- webflux-stock-quotes/src/main/java/com/singletonapps/webflux/webfluxstockquotes/QuoteRouter.java
+++ webflux-stock-quotes/src/main/java/com/singletonapps/webflux/webfluxstockquotes/QuoteRouter.java
@@ -6,7 +6,7 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
@@ -17,7 +17,9 @@ public class QuoteRouter {
 
         return RouterFunctions
                 .route(GET("/hello").and(accept(TEXT_PLAIN)), quoteHandler::hello)
-                .andRoute(POST("/echo").and(accept(TEXT_PLAIN).and(contentType(TEXT_PLAIN))), quoteHandler::echo);
+                .andRoute(POST("/echo").and(accept(TEXT_PLAIN).and(contentType(TEXT_PLAIN))), quoteHandler::echo)
+                .andRoute(GET("/quotes").and(accept(APPLICATION_JSON)), quoteHandler::fetchQuotes)
+                .andRoute(GET("/quotes").and(accept(APPLICATION_STREAM_JSON)), quoteHandler::streamQuotes);
     }
 
 }
