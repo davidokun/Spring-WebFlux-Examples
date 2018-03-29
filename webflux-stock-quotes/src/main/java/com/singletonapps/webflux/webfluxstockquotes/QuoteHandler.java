5b55339cefc7e274b8894b8cab1553f288782411
davidokun <davidokun99@gmail.com>
2018-03-29 13:37:20 -0500

    Adding functional endpoint /hello

---
+++ webflux-stock-quotes/src/main/java/com/singletonapps/webflux/webfluxstockquotes/QuoteHandler.java
@@ -0,0 +1,21 @@
+package com.singletonapps.webflux.webfluxstockquotes;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Component
+public class QuoteHandler {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+
+        return ok().contentType(TEXT_PLAIN)
+                .body(BodyInserters.fromObject("Hello Webflux!!"));
+    }
+
+}
