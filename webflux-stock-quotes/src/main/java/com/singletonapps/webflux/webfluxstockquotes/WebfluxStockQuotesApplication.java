8c97ee20f0f9b31016267a056674e8210a54350b
davidokun <davidokun99@gmail.com>
2018-03-29 12:47:58 -0500

    Initial project setup.

---
+++ webflux-stock-quotes/src/main/java/com/singletonapps/webflux/webfluxstockquotes/WebfluxStockQuotesApplication.java
@@ -0,0 +1,12 @@
+package com.singletonapps.webflux.webfluxstockquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStockQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxStockQuotesApplication.class, args);
+    }
+}
